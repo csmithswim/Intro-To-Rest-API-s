750db396f828c140773cc9c731f10c1920156e77
Cullen Smith <cullensmi@gmail.com>
2020-05-26 22:16:34 -0400

    may26 rest api project

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ const express = require('express'), //require function defines express
 
     reqBodyLog = require('./middleware/reqBodyLog');
 
+require('dotenv').config();
+
+//updating database with read/write file. Creating our database on our text.
+
+// const fs = require('fs')
+
+// let textFile = process.cwd() + '/database/database.text';
+
+// let parsedData = fs.readFileSync(textFile, 'utf8'); 
+
+// if (parsedData[0] != '{' || parsedData[parsedData.length-1] != '}') {
+//     parsedData = '{}'
+// }
+
+// parsedData = JSON.parse(parsedData);
+
+// parsedData.movies = database.movies;
+
+// console.log(parsedData);
+
+// parsedData = JSON.stringify(parsedData);
+
+// fs.writeFileSync(textFile, parsedData);
+
+//end of code
+
 const app = express() //represents application
 
 const port = process.env.PORT || 3000; //global object process
@@ -20,22 +46,9 @@ const userRouter = require('./routes/usersRouter');
 
 app.use('/users', userRouter);
 
-let database = {
-    allMovies: [
+const movieRouter = require('./routes/movieRouter') //file name  
 
-        {title: 'Looper',             release: 2012, available: false, imbdLink: 'https://www.imdb.com/title/tt1276104/', img: 'https://upload.wikimedia.org/wikipedia/en/0/0a/Looper_poster.jpg' }, 
-        {title: 'Back To The Future', release: 1985, available: true, imbdLink: 'https://www.imdb.com/title/tt0088763/', img: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg'}, 
-        {title: 'Inception',          release: 2010, available: false, imbdLink: 'https://www.imdb.com/title/tt1375666/', img: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'}, 
-        {title: 'Donnie Darko',       release: 2001, available: true, imbdLink: 'https://www.imdb.com/title/tt0246578/', img: 'https://m.media-amazon.com/images/M/MV5BOGRiOGM5MmUtMmI3Yi00ZTFhLTlhZDYtZGNmOWRmYTM4NWE2XkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_SY1000_CR0,0,702,1000_AL_.jpg' }, 
-        {title: 'Primer',             release: 2004, available: true, imbdLink: 'https://www.imdb.com/title/tt0390384/', img: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Primer_%282004_film_poster%29.jpg' }, 
-        {title: 'Terminator 2',       release: 1991, available: true, imbdLink: 'https://www.imdb.com/title/tt0103064/', img: 'https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg' },
-        {title: 'Source Code',        release: 2011, available: true, imbdLink: 'https://www.imdb.com/title/tt0945513/', img: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Source_Code_Poster.jpg/220px-Source_Code_Poster.jpg' },
-        {title: 'Déjà Vu',            release: 2006, available: false, imbdLink: 'https://www.imdb.com/title/tt0453467/', img: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/DejaVuBigPoster.jpg/220px-DejaVuBigPoster.jpg' }
- 
-    ],
-
-    users: []
-}
+app.use('/movie', movieRouter);
 
 app.get('/', reqBodyLog, (req ,res) => {
 
