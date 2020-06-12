2ea901d2cd30cca3f28afc20daa2f34cf0acdb24
Cullen Smith <cullensmi@gmail.com>
2020-06-12 11:36:57 -0400

    june 12 commit of index.js of movie DB project

--- firstMongoProject/juneWT/index.js
+++ firstMongoProject/juneWT/index.js
@@ -16,15 +16,20 @@ const express = require('express'),
 
     homeRouter = require('./routes/homeRouter');
 
+    movieRouter = require('./routes/movieRouter');
+
 
     //some middleware needs to go before others
-    server.use(morgan('dev'))
+    server.use(morgan('dev'));
 
     server.use(express.json());
 
-
+    server.use(express.static('./public'));
+    
     //express.json and morgan must be called before homerouter
     server.use('/', homeRouter);
+
+    server.use('/movie', movieRouter)//We do not need to specify movie in our movieRouter program
 
 
       mongoose.connect(connectionURI, deprecatedObj, () => {
