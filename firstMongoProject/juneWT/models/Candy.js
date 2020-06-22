3656d965cbe495a4b03e4719ea2f12ab9bc405f5
Cullen Smith <cullensmi@gmail.com>
2020-06-22 11:45:56 -0400

    june22 commit of Candy module

---
+++ firstMongoProject/juneWT/models/Candy.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const Candy = new mongoose.Schema({
+
+    name: {
+        unique: true,
+        required: true,
+        type: String        
+    },
+    calories: {
+        required: true,
+        type: Number
+    }, 
+    type: {
+        required: true, 
+        type: String,
+    },
+    img: {
+        required: true,
+        type: String,
+        validate: (value) => {
+
+            const test = !validator.isURL(value)
+
+            if (test) {
+
+                throw new Error('Image Link Not Valid')
+            }
+        }
+    },
+
+    inventory: {
+
+        required: false,
+
+        type: Object,
+
+        default: {
+
+                available: 1, //giving it a type Number is for validation when you give a post request.
+                rented: 0
+        }
+    }
+})
+
+module.exports = mongoose.model('Candy', Candy); //What the collection gets called.
