624a7b0714f0a962b72c420b9df709d9a145ee99
Cullen Smith <cullensmi@gmail.com>
2020-07-15 21:20:24 -0400

    edited login user middleware

--- firstMongoProject/juneWT/middleware/loginUser.js
+++ firstMongoProject/juneWT/middleware/loginUser.js
@@ -1,51 +1,66 @@
-const User = require("../models/User");
-const validator = require("validator");
-const bcrypt = require("bcrypt");
-
+const bcrypt = require("bcrypt"),
+    validator = require("validator"),
+    User = require("../models/User");
 
 const failedLogin = (req, res) => {
-    return res.status(409).json({message: "Login Failed"});
-}
 
-module.exports = async (req, res, next) => {
+    return res.status(409).json({ msg: "Login Failed" });
+
+};
+
+module.exports = async(req, res, next) => {
 
     try {
+
         const email = req.body.email,
-              emailValid = 
-                (email === undefined || email.trim() === '')
-                    ? false
-                    : validator.isEmail(email);
+            validEmail = (email === undefined || email.trim() === '') ?
+            false :
+            validator.isEmail(email);
 
-        if (!emailValid) {
+        if (!validEmail) {
+
             console.error('\nLogin Failed: Email Not Valid');
-            failedLogin()    
-        }
 
-        const user = await User.findOne({email: req.body.email});
+            failedLogin();
+
+        };
+
+        const user = await User.findOne({ email: req.body.email });
 
         if (user === null) {
-            console.error('\nLogin Failed: Email Not In Use');
-            failedLogin()
-        }
-        const pass = req.body.password,
-              passTest = 
-                (pass === undefined || pass.trim() === '') 
-                    ? false 
-                    : await bcrypt.compare(pass, user.password);
 
-        if (!passTest) {
+            console.error('\nLogin Failed: Email Not In Use');
+
+            failedLogin();
+
+        };
+
+        const pass = req.body.password,
+            testPass = (pass === undefined || pass.trim() === '') ?
+            false :
+            await bcrypt.compare(pass, user.password);
+            
+            if (!testPass) {
+
             console.error('\nLogin Failed: Password Invalid');
-            failedLogin()
-        }
+
+            failedLogin();
+
+        };
 
         req.id = user._id;
 
-        next() //if code execution reaches here, it is assumed the user has successfully logged in
-        
+        next();
+
     } catch (err) {
-        res.status(500).json({
+
+        return res.status(500).json({
+
             errorAt: err.stack,
-            message: err.message || 'err'
-        })
-    }
-}
+            msg: err.message || err
+
+        });
+
+    };
+
+};
