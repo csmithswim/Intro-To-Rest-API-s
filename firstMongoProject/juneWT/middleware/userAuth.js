9db64c6e24aaae946df75af09b09a0c6cf2c1971
Cullen Smith <cullensmi@gmail.com>
2020-07-15 21:23:02 -0400

    user auth midelware

--- firstMongoProject/juneWT/middleware/userAuth.js
+++ firstMongoProject/juneWT/middleware/userAuth.js
@@ -4,15 +4,32 @@ module.exports = async (req, res, next) => {
 
     const {JWT_SECRET: jwtKey, HEAD_AUTH_KEY: headerKey} = process.env;
 
-    console.log(jwtKey, headerKey);
-   
     const userToken = req.headers[headerKey];
 
-    const decode = jwt.verify(userToken, jwtKey)
+    try {
 
-    console.log(decode);
+        const decodedData = jwt.verify(userToken, jwtKey);
 
-    next()
+        if (decodedData.id === undefined) { 
+
+            throw new Error('Id was not defined in the payload');
+        }
+
+        req.userId = decodedData.id;
+
+        next()
+
+    } catch (err) {
+
+        const errMsg = err.message || err;
+
+        console.log(`\nError in UserAuth: ${errMsg}\n`);
+
+        return res.status(401).json({error: 'Not Authorized'})
+    }
+
+    // console.log(decode);
+
 
 }
 
