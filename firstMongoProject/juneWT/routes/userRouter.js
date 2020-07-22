94685e7e8e36e7e8742c44ee75fd421e522e7b65
Cullen Smith <cullensmi@gmail.com>
2020-07-21 21:14:20 -0400

    updated userrouter middleware

--- firstMongoProject/juneWT/routes/userRouter.js
+++ firstMongoProject/juneWT/routes/userRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const Movie = require("../models/Movie");
 const secret = process.env.JWT_SECRET;
 
 const validateUser = require('../middleware/validateUser');
@@ -12,11 +12,87 @@ const loginUser = require('../middleware/loginUser');
 const userAuth = require('../middleware/userAuth');
 const adminAuth = require('../middleware/adminAuth');
 
+const newError = require('../utils/newError');
+
+
 //Post route for users
 //localhost: 3001/user
 //@desc post/make a new user and store in users collection
 //@patch (server path)/user/post
 //@access admin level 
+
+router.patch(
+    '/rent',
+    userAuth,
+    async (req, res) => {
+
+
+    const movieId = req.body.movieId;
+
+    try {
+
+        const movie = await Movie.findOne({_id: movieId, 'inventory.available': {$gte: 1} });
+    
+        console.log(`\nFound Movie: ${movie}\n`);
+
+        if (movie === null) {
+            
+        console.log(`Movie Id caused error error renting ${movieId}`);
+
+        throw newError('Movie Not Found or Movie Unavailable', 404);
+
+        }
+
+        //check if the user is already renting
+        if (req.user.rentedMovies.indexOf(movieId) != -1) {
+
+            console.log(`User tried rented movie twice\n movieId: ${movieId}
+            \nUserId: ${req.user._id}`);
+            throw newError('Movie Not Found or Movie Unavailable', 409);
+
+        }
+
+        // modify the user doc 
+        await User.updateOne(
+            {_id: req.user._id},
+            { $addToSet: { rentedMovies: movieId} }
+        )
+
+
+        //modifying the movie doc
+        await Movie.updateOne(
+            {_id: movieId}, 
+            {
+                $addToSet: {'inventory.rented': req.user._id},
+
+                $inc: {'inventory.available': -1},
+            },
+            {new:1}
+            
+            )
+            res.status(200).json({
+                message: "Patch Successful"
+            })
+
+        
+    } catch (err) {
+
+        console.log(err.message.msg, err.message.status);
+
+        const errMsg = err.message || err;
+
+        console.log(`Error in movie renting: ${errMsg}`);
+
+        res.status(500).json({
+            error: errMsg
+
+        })
+
+    }
+
+    }
+)
+
 
 router.get(
     '/testAuth',
