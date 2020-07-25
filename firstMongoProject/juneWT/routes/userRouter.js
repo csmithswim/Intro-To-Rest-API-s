4f79dadeee5ea961ed566cc2faaea5987684257c
Cullen Smith <cullensmi@gmail.com>
2020-07-24 21:18:35 -0400

    updated userrouter

--- firstMongoProject/juneWT/routes/userRouter.js
+++ firstMongoProject/juneWT/routes/userRouter.js
@@ -15,83 +15,88 @@ const adminAuth = require('../middleware/adminAuth');
 const newError = require('../utils/newError');
 
 
+
+//movie renting & returning 
+
+//movie renting & returning 
+router.patch(
+    '/rent_or_return',
+    userAuth,
+    async (req, res) => {
+        
+        const {movieId, isRenting = true} = req.body;
+
+        try {
+
+            const movieQuery = 
+                    isRenting 
+                    ? { _id: movieId, 'inventory.available': { $gte: 1 }} 
+                    : { _id: movieId };
+
+            const userUpdate = 
+                    isRenting 
+                    ? { $addToSet: { rentedMovies: movieId} } 
+                    : { $pull: { rentedMovies: movieId} };
+
+            const movieUpdate = 
+                    isRenting 
+                    ? { $addToSet: { 'inventory.rented': req.user._id }, $inc: { 'inventory.available': -1 }} 
+                    : { $pull: { 'inventory.rented': req.user._id }, $inc: { 'inventory.available': 1 }};
+
+            const movie = await Movie.findOne(movieQuery);
+
+            if (movie === null) {
+                console.log(`Movie Id caused error renting ${movieId}`);
+                throw newError('Movie Not Found or Movie Unavailable', 404);
+            }
+            
+            //ToDo
+            //if the user is renting, make sure they dont already have that movie rented
+			//if the user is returning, make sure they are currently renting the movie
+            
+
+            //modify the user doc
+            const newUser = await User.findByIdAndUpdate(
+                req.user._id,
+                userUpdate,
+                {new: 1}
+            )
+
+            //modifying the movie doc
+            const newMovie = await Movie.findByIdAndUpdate(
+                movieId,
+                movieUpdate,
+                {new: 1}
+            )
+
+            res.json({
+                message: "successs",
+                user: newUser,
+                movie: newMovie
+            })
+            
+        } catch (err) {
+            const errMsg = err.message || err;
+            const errCode = err.code || 500;
+
+            console.log(`Error in movie renting: ${errMsg}`);
+            
+            res.status(errCode).json({
+                error: errMsg
+            })
+        }
+
+    }
+)
+
+
 //Post route for users
 //localhost: 3001/user
 //@desc post/make a new user and store in users collection
 //@patch (server path)/user/post
 //@access admin level 
 
-router.patch(
-    '/rent',
-    userAuth,
-    async (req, res) => {
 
-
-    const movieId = req.body.movieId;
-
-    try {
-
-        const movie = await Movie.findOne({_id: movieId, 'inventory.available': {$gte: 1} });
-    
-        console.log(`\nFound Movie: ${movie}\n`);
-
-        if (movie === null) {
-            
-        console.log(`Movie Id caused error error renting ${movieId}`);
-
-        throw newError('Movie Not Found or Movie Unavailable', 404);
-
-        }
-
-        //check if the user is already renting
-        if (req.user.rentedMovies.indexOf(movieId) != -1) {
-
-            console.log(`User tried rented movie twice\n movieId: ${movieId}
-            \nUserId: ${req.user._id}`);
-            throw newError('Movie Not Found or Movie Unavailable', 409);
-
-        }
-
-        // modify the user doc 
-        await User.updateOne(
-            {_id: req.user._id},
-            { $addToSet: { rentedMovies: movieId} }
-        )
-
-
-        //modifying the movie doc
-        await Movie.updateOne(
-            {_id: movieId}, 
-            {
-                $addToSet: {'inventory.rented': req.user._id},
-
-                $inc: {'inventory.available': -1},
-            },
-            {new:1}
-            
-            )
-            res.status(200).json({
-                message: "Patch Successful"
-            })
-
-        
-    } catch (err) {
-
-        console.log(err.message.msg, err.message.status);
-
-        const errMsg = err.message || err;
-
-        console.log(`Error in movie renting: ${errMsg}`);
-
-        res.status(500).json({
-            error: errMsg
-
-        })
-
-    }
-
-    }
-)
 
 
 router.get(
@@ -154,7 +159,7 @@ router.put(
     loginUser,    
     (req, res) => {        
 
-        console.log(req.id)
+        // console.log(req.id)
         
         const token = jwt.sign({id: req.id}, secret, {expiresIn: '1h'});
 
