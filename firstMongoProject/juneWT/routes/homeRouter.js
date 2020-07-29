f766a8d3c48c1cb0f993d8286dc8dde067b3f98d
Cullen Smith <cullensmi@gmail.com>
2020-07-28 22:15:02 -0400

    updated homerouter middleware

--- firstMongoProject/juneWT/routes/homeRouter.js
+++ firstMongoProject/juneWT/routes/homeRouter.js
@@ -5,7 +5,7 @@ const Candy = require('../models/Candy');
 const adminAuth = require('../middleware/adminAuth');
 const extractToken = require('../middleware/extractToken');
 const User = require('../models/User');
-
+const isAdmin = require('../middleware/isAdmin');
 
 //By default it is presumed that the views folder will have the pug files.
 
@@ -23,16 +23,24 @@ router.get('/movierental/static', (req, res) =>  {
     res.sendFile(fileLoc)
 })
 
-router.get('/', extractToken, async (req, res) => {
+router.get('/', 
+extractToken, 
+isAdmin, 
+async (req, res) => {
 
     const loggedIn = req.authKey != undefined;
-    const isAdmin = await User.find({})
 
     const allMovies = await Movie.find({ 'inventory.available': {$gte: 1}} );
 
-        clientMsg = 'Number of Movies: ' + allMovies.length;
+    const isAdmin = req.isAdmin || false;
 
-    res.render('home', {all_movies: allMovies, message: clientMsg, isLoggedIn: loggedIn})
+    const renderOption = {
+        all_movies: allMovies,
+        isLoggedIn: loggedIn,
+        isAdmin: isAdmin
+    }
+
+    res.render('home', renderOption)
 })
 
 router.get('/candy', async (req, res) => {
@@ -44,7 +52,9 @@ router.get('/candy', async (req, res) => {
     res.render('candy', {all_candy: allCandy, message: clientMsg})
 })
 
-router.get('/mrental/admin/:key', adminAuth, (req, res) => {
+router.get('/admin',
+
+adminAuth, (req, res) => {
 
     res.render('admin-movie')
 
