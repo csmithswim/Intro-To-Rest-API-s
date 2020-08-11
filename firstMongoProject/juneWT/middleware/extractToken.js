7187b84319480d42e2fb5446eb21e2b931d551ad
Cullen Smith <cullensmi@gmail.com>
2020-08-10 20:46:34 -0400

    updated extract token

--- firstMongoProject/juneWT/middleware/extractToken.js
+++ firstMongoProject/juneWT/middleware/extractToken.js
@@ -1,12 +1,13 @@
 const cookie = require('cookie');
 
-module.exports = (req, res, next) => {
+module.exports = (req, res, next) => { //Changing the format from cookie(large string) to JSON key/value pairs
 
     const parsed = cookie.parse(req.headers.cookie || "");
 
     const token = parsed.token;
 
-    req.authKey = token;
+    req.authKey = token; //Sets to request property to be used in any middleware function
 
     next();
 }
+
